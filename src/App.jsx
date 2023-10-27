2a16c6c2c8b62bd0bdef5b5351fecda6bf91dd8c
Vinh Tieu <tieuvinh32@gmail.com>
2023-10-27 09:39:39 +0700

    Added Login Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
