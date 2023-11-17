bf3a00af0155117745e17973a4926237f292ac21
Vinh Tieu <tieuvinh32@gmail.com>
2023-11-17 16:08:57 +0700

    Fixed Loading Screen Bug

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -14,7 +14,7 @@ export default function Layout({ children }) {
           isNavMenuOpen || isLoading ? "overflow-hidden w-screen h-screen" : ""
         } min-h-screen flex flex-col `}>
         <CSSTransition
-          in={true}
+          in={isLoading}
           timeout={{
             appear: 1000,
             enter: 1000,
